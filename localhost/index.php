1eb604a4d73abe48689439a6524865fb2272e141
Helbushek <gorevaya134@gmail.com>
2024-01-05 00:30:39 +0700

    new servered site

---
+++ localhost/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+<?php 
+    define(MY_AGE, 24);
+    echo $MY_AGE . "<br>";
+    $number = 5; 
+    //$number = 45;
+    $num = -0.55;
+    $bool = true;
+
+    $a = 0.5;
+    $b = "0.5";
+    
+    echo $b+ $a;
+?>
+</body>
+
+</html>
+
