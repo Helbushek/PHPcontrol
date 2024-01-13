6cd9521b2c596856858626f8b4709463148022e4
Helbushek <gorevaya134@gmail.com>
2024-01-13 13:59:21 +0700

    hihihi hahaha

--- example-app/app/Providers/AuthServiceProvider.php
+++ example-app/app/Providers/AuthServiceProvider.php
@@ -5,6 +5,9 @@ namespace App\Providers;
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
+
 class AuthServiceProvider extends ServiceProvider
 {
     /**
@@ -22,5 +25,10 @@ class AuthServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        Gate::define('viewPulse', function (User $user) {
+            return $user->isAdmin();
+        });
     }
+
+    
 }
---
+++ example-app/resources/views/about.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title-block')О нас@endsection
+
+@section('content')
+<h1>Страница про нас</h1>
+@endsection
+
